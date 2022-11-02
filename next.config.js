ffed3f3522c86f6749e18698d63be0753752c9fa
Elumion <dovgalyukvlad87@gmail.com>
2022-11-02 22:49:03 +0200

    change pathname

--- next.config.js
+++ next.config.js
@@ -3,13 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    minimumCacheTTL: 1,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "picsum.photos",
         port: "",
-        pathname: "**",
+        pathname: "/200/**",
       },
     ],
   },
