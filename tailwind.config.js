cf8728cded80a7b7cfe1591077935ca71b123f64
Elumion <dovgalyukvlad87@gmail.com>
2022-11-02 22:49:33 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        item: "2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12);",
+      },
+      screens: {
+        xl: "1400px",
+      },
+    },
+  },
+  plugins: [],
+};
