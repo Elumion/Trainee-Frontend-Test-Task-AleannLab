2e2d950e6bf5fb386aeb1a8c5648fd623451e364
Elumion <dovgalyukvlad87@gmail.com>
2022-11-05 23:14:34 +0200

    add new font definition and change screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,12 @@ module.exports = {
         item: "2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12);",
       },
       screens: {
-        xl: "1400px",
+        xl: { max: "1400px" },
       },
       fontFamily: {
         roboto: "Roboto",
+        proxima: "Proxima Nova Rg",
+        semibold: "Proxima Nova Lt",
       },
     },
   },
