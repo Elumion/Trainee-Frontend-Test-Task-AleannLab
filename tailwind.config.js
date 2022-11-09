3d1343acb2dcc60724c02bd87349fe58c0243458
Elumion <dovgalyukvlad87@gmail.com>
2022-11-10 01:18:29 +0200

    add screens for max-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
     extend: {
       boxShadow: {
         item: "2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12);",
+        pagination:
+          "2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12)",
       },
       screens: {
         xl: { max: "1400px" },
+        lg: { max: "1200px" },
+        l: { max: "1000px" },
+        md: { max: "780px" },
+        sm: { max: "420px" },
       },
       fontFamily: {
         roboto: "Roboto",
